d16d645498e93482776a8e15511856233bac2a62
jsl1618 <163863542+jsl1618@users.noreply.github.com>
2024-04-11 16:14:46 -0400

    Add files via upload

--- FindMyFriend.py
+++ FindMyFriend.py
@@ -1,50 +1,93 @@
 # PyTorch train script
 # https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html
 
-# We will use the CIFAR10 dataset: https://www.cs.toronto.edu/~kriz/cifar.html
+# We will use the Custom Dataset
 
 import torch
 from torch import nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor, Normalize, Grayscale
+import os
+from PIL import Image
 
-transform=transforms.Compose([
-    transforms.Resize((28,28)),
-    Grayscale(num_output_channels=1),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,),(0.5,))
+class CustomDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.classes = os.listdir(root_dir)
+        self.class_to_idx = {cls_name: i for i, cls_name in enumerate(self.classes)}
+        self.images = self._load_images()
+
+    def _load_images(self):
+        images = []
+        for cls_name in self.classes:
+            class_dir = os.path.join(self.root_dir, cls_name)
+            for img_name in os.listdir(class_dir):
+                img_path = os.path.join(class_dir, img_name)
+                images.append((img_path, self.class_to_idx[cls_name]))
+        return images
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path, label = self.images[idx]
+        image = Image.open(img_path).convert('RGB')
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+# Define transformations to be applied to the images
+transform = transforms.Compose([
+    transforms.Resize((50, 50)),  # Resize to a uniform size
+    transforms.ToTensor(),        # Convert image to tensor
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize
 ])
 
-training_data = datasets.CIFAR10(
-    root="data",
-    train=True,
-    download=True,
-    transform=transform
-)
+# transform=transforms.Compose([
+#     transforms.Resize((50,50)),
+#     Grayscale(num_output_channels=1),
+#     transforms.ToTensor(),
+#     transforms.Normalize((0.5,),(0.5,))
+# ])
 
-test_data = datasets.CIFAR10(
-    root="data",
-    train=False,
-    download=True,
-    transform=transform
-)
+# Define your custom dataset
+custom_dataset_train = CustomDataset(root_dir='C:/Users/HP/Downloads/FindMyFriend/Train', transform=transform)
+custom_dataset_test=CustomDataset(root_dir='C:/Users/HP/Downloads/FindMyFriend/Test', transform=transform)
+
+# Create dataloaders for training and testing
+train_dataloader = DataLoader(custom_dataset_train, batch_size=27, shuffle=True)
+test_dataloader = DataLoader(custom_dataset_test, batch_size=5, shuffle=False)
+#
+# training_data = datasets.CIFAR10(
+#     root="data",
+#     train=True,
+#     download=True,
+#     transform=transform
+# )
+
+# test_data = datasets.CIFAR10(
+#     root="data",
+#     train=False,
+#     download=True,
+#     transform=transform
+# )
 
 # let us print some data:
 
-categories = ['airplane', 'automobile', 'bird', 'cat', 'deer', 
-                'dog', 'frog', 'horse', 'ship', 'truck']
+categories = ['Erica', 'Dylan', 'Abby', 'Kasey', 'Justine']
 
 # select a random sample from the training set
 sample_num = 143
-print(training_data[sample_num])
-print('Inputs sample - image size:', training_data[sample_num][0].shape)
-print('Label:', training_data[sample_num][1], '\n')
+print(train_dataloader[sample_num])
+print('Inputs sample - image size:', train_dataloader[sample_num][0].shape)
+print('Label:', train_dataloader[sample_num][1], '\n')
 
 import matplotlib.pyplot as plt
 
-ima = training_data[sample_num][0]
+ima = train_dataloader[sample_num][0]
 print('Inputs sample - min,max,mean,std:', ima.min().item(), ima.max().item(), ima.mean().item(), ima.std().item())
 ima = (ima - ima.mean())/ ima.std()
 print('Inputs sample normalized - min,max,mean,std:', ima.min().item(), ima.max().item(), ima.mean().item(), ima.std().item())
@@ -55,7 +98,7 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.flatten = nn.Flatten()
-        self.l1 = nn.Linear(28*28, 512)
+        self.l1 = nn.Linear(50*50*3, 512)
         self.l2 = nn.Linear(512, 512)
         self.l3 = nn.Linear(512, 10)
 
@@ -63,7 +106,6 @@ class Net(nn.Module):
         #print("input shape:", x.shape)
         x = self.flatten(x)
         x = F.relu(self.l1(x))
-        #x = F.relu(self.l1(x.view(x.size(0), -1)))
         x = F.relu(self.l2(x))
         output = self.l3(x)
         #print("Shape of l1 weights:", self.l1.weight.shape)
@@ -109,8 +151,8 @@ def test_loop(dataloader, model, loss_fn):
 
 model = Net()
 
-train_dataloader = DataLoader(training_data, batch_size=64)
-test_dataloader = DataLoader(test_data, batch_size=64)
+train_dataloader = DataLoader(train_dataloader, batch_size=64)
+test_dataloader = DataLoader(test_dataloader, batch_size=64)
 
 learning_rate = 1e-3
 batch_size = 64
@@ -129,7 +171,7 @@ print("Done!")
 sample_num = 143 # select a random sample
 
 with torch.no_grad():
-    r = model(training_data[sample_num][0])
+    r = model(train_dataloader[sample_num][0])
 
 print('neural network output pseudo-probabilities:', r)
 print('neural network output class number:', torch.argmax(r).item())
